531af7afa9d01e0df1258ef77593e33ba6d6498f
John Vea <69362544+JohnVea@users.noreply.github.com>
2024-08-27 18:15:08 -0400

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import CustomerDashboardPage from './pages/CustomerDashboardPage';
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/registration" element={<SignupPage/>} />
+        <Route path="/CustomerDashboardPage" element={<CustomerDashboardPage />} />
       </Routes>
     </BrowserRouter>
   );
